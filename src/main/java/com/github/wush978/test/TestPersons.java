bf0ff6de5fbdff2bc5fb47ff10458749bc8518c5
Wush Wu <wush.wu@generalmobi.com>
2016-11-17 16:19:24 +0800

    refactor

--- src/main/java/com/github/wush978/test/TestPersons.java
+++ src/main/java/com/github/wush978/test/TestPersons.java
@@ -1,5 +1,9 @@
 package com.github.wush978.test;
 
+import com.github.wush978.vectorizer.Vector;
+import com.google.common.collect.Multiset;
+import com.google.common.collect.TreeMultiset;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,5 +35,74 @@ public class TestPersons {
                 result.get(0).getContact().toBuilder().setAddress("example address")
         ).build());
         return result;
+    }
+
+    private static class Entry implements Comparable<Entry> {
+
+        String k;
+
+        Double v;
+
+        Entry() {
+            this.k = null;
+            this.v = null;
+        }
+
+        Entry(String k, Double v) {
+            this.k = k;
+            this.v = v;
+        }
+
+        public String getKey() {
+            return k;
+        }
+
+        public Double getValue() {
+            return v;
+        }
+
+        public String setKey(String key) {
+            k = key;
+            return key;
+        }
+
+        public Double setValue(Double value) {
+            v = value;
+            return value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof Entry) {
+                return compareTo((Entry) obj) == 0;
+            }
+            return false;
+        }
+
+        public int compareTo(Entry o) {
+            int result = k.compareTo(o.k);
+            if (result == 0) {
+                return v.compareTo(o.v);
+            }
+            return result;
+        }
+    }
+
+    public static boolean equalWithoutOrder(Vector.SparseVector a, Vector.SparseVector b) throws RuntimeException {
+        List<String> sa = a.getIndexList(), sb = b.getIndexList();
+        if (sa.size() != sb.size()) return false;
+        List<Double> da = a.getValueList(), db = b.getValueList();
+        if (da.size() != sa.size() | db.size() != sb.size()) {
+            throw new RuntimeException("The index and value are inconsistent");
+        }
+        Multiset<Entry>
+                l1 = TreeMultiset.create(),
+                l2 = TreeMultiset.create();
+
+        for(int i = 0;i < sa.size();i++) {
+            l1.add(new Entry(sa.get(i), da.get(i)));
+            l2.add(new Entry(sb.get(i), db.get(i)));
+        }
+        return l1.equals(l2);
     }
 }
--- src/main/test/com/github/wush978/reference/VectorizerTest.java
+++ src/main/test/com/github/wush978/reference/VectorizerTest.java
@@ -17,81 +17,14 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
+import static com.github.wush978.test.TestPersons.equalWithoutOrder;
+
 /**
  * Created by wush on 2016/11/9.
  */
 
 @RunWith(JUnit4.class)
 public class VectorizerTest {
-
-    private class Entry implements Comparable<Entry> {
-
-        String k;
-
-        Double v;
-
-        Entry() {
-            this.k = null;
-            this.v = null;
-        }
-
-        Entry(String k, Double v) {
-            this.k = k;
-            this.v = v;
-        }
-
-        public String getKey() {
-            return k;
-        }
-
-        public Double getValue() {
-            return v;
-        }
-
-        public String setKey(String key) {
-            k = key;
-            return key;
-        }
-
-        public Double setValue(Double value) {
-            v = value;
-            return value;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (obj instanceof Entry) {
-                return compareTo((Entry) obj) == 0;
-            }
-            return false;
-        }
-
-        public int compareTo(Entry o) {
-            int result = k.compareTo(o.k);
-            if (result == 0) {
-                return v.compareTo(o.v);
-            }
-            return result;
-        }
-    }
-
-    boolean equalWithoutOrder(Vector.SparseVector a, Vector.SparseVector b) throws RuntimeException {
-        List<String> sa = a.getIndexList(), sb = b.getIndexList();
-        if (sa.size() != sb.size()) return false;
-        List<Double> da = a.getValueList(), db = b.getValueList();
-        if (da.size() != sa.size() | db.size() != sb.size()) {
-            throw new RuntimeException("The index and value are inconsistent");
-        }
-        Multiset<Entry>
-            l1 = TreeMultiset.create(),
-            l2 = TreeMultiset.create();
-
-        for(int i = 0;i < sa.size();i++) {
-            l1.add(new Entry(sa.get(i), da.get(i)));
-            l2.add(new Entry(sb.get(i), db.get(i)));
-        }
-        return l1.equals(l2);
-    }
 
     @Test
     public void testVectorizer() {
